f4165ac898fff1f27223882e4af426e411f15ddc
Raymond Mojica <rm@M12020.home>
2022-08-16 00:46:34 -0400

    context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Gallery from "./components/Gallery";
 import SearchBar from "./components/SearchBar";
+import { DataContext } from "./context/DataContext";
 
 function App() {
   let [search, setSearch] = useState("");
@@ -23,11 +24,18 @@ function App() {
     fetchData();
   }, [search]);
 
+  const handleSearch = (e, term) => {
+    e.preventDefault();
+    setSearch(term);
+  };
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar handleSearch={handleSearch} />
       {message}
-      <Gallery />
+      <DataContext.Provider value={data}>
+        <Gallery />
+      </DataContext.Provider>
     </div>
   );
 }
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,11 +1,14 @@
+import { useContext } from "react";
+import { DataContext } from "../context/DataContext";
 import GalleryItem from "./GalleryItem";
 
 function Gallery(props) {
-  return (
-    <div>
-      <GalleryItem />
-    </div>
-  );
+  const data = useContext(DataContext);
+
+  const display = data.map((item, index) => {
+    return <GalleryItem item={item} key={index} />;
+  });
+  return <div>{display}</div>;
 }
 
 export default Gallery;
